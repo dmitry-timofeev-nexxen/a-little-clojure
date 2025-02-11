777ae68b07a1b4a3d0e40d1f7c71ea00e9b62aec
dtimofeev <dtimofeev@nexxen.com>
2025-02-11 20:02:09 +0300

    Complete exercises from Chapter 13

--- test/a_little_clojure/core_test.clj
+++ test/a_little_clojure/core_test.clj
@@ -1,7 +1,22 @@
 (ns a-little-clojure.core-test
   (:require [clojure.test :refer :all]
-            [a-little-clojure.core :refer :all]))
+            [a-little-clojure.core :refer [square]]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest test-addition
+  (testing "Addition function"
+    (is (= 4 (+ 2 2)))
+    (is (= 10 (+ 5 5)))))
+
+(deftest test-string
+  (testing "String operations"
+    (is (= "HELLO" (.toUpperCase "hello")))
+    (is (= "world" (.substring "hello world" 6)))))
+
+(deftest test-custom-function
+  (testing "Testing a custom function from core.clj"
+    (is (= 9 (square 3)))
+    (is (= 16 (square 4)))))
